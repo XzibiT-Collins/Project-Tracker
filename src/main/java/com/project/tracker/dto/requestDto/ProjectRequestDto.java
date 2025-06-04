9acc20c2bb68e820d9e76b06f201a40b97297304
Huvison <huvisoncollins@gmail.com>
2025-06-04 16:03:55 +0000

    refactor: (feat/models) update DTO's to Records

--- src/main/java/com/project/tracker/dto/requestDto/ProjectRequestDto.java
+++ src/main/java/com/project/tracker/dto/requestDto/ProjectRequestDto.java
@@ -3,25 +3,19 @@ package com.project.tracker.dto.requestDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Date;
 
-@Getter
-@Setter
-@Builder
-public class ProjectRequestDto {
+public record ProjectRequestDto(
     @NotBlank(message = "Project name must not be blank.")
-    private String projectName;
+    String projectName,
 
     @Size(max = 500, message = "Description cannot be more than 500 characters")
-    private String description;
+    String description,
 
     @NotNull(message = "Project must have a deadline.")
-    private Date deadline;
+    Date deadline,
 
     @NotBlank(message = "Project must have a status.")
-    private String status;
-}
+    String status
+) {}
--- src/main/java/com/project/tracker/dto/requestDto/TaskRequestDto.java
+++ src/main/java/com/project/tracker/dto/requestDto/TaskRequestDto.java
@@ -3,30 +3,24 @@ package com.project.tracker.dto.requestDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Date;
 
-@Getter
-@Setter
-@Builder
-public class TaskRequestDto {
-    @NotBlank(message = "Task must have a title.")
-    private String title;
+public record TaskRequestDto(
+        @NotBlank(message = "Task must have a title.")
+        String title,
 
-    @Size(max = 500, message = "Description cannot be more than 500 characters.")
-    private String description;
+        @Size(max = 500, message = "Description cannot be more than 500 characters.")
+        String description,
 
-    @NotBlank(message = "Task must have a status.")
-    private String status;
+        @NotBlank(message = "Task must have a status.")
+        String status,
 
-    @NotNull(message = "Task must have a due date.")
-    private Date dueDate;
+        @NotNull(message = "Task must have a due date.")
+        Date dueDate,
 
-    private int developerId;
+        int developerId,
 
-    @NotNull(message = "Task must belong to a project.")
-    private int projectId;
-}
+        @NotNull(message = "Task must belong to a project.")
+        int projectId
+) {}
