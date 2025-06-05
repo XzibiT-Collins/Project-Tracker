5f649bc4f662f97e6a9e9c1082c9da861ae21068
Huvison <huvisoncollins@gmail.com>
2025-06-05 08:02:06 +0000

    refactor: (feat/models) update repository to include pagination

--- src/main/java/com/project/tracker/repositories/DeveloperRepository.java
+++ src/main/java/com/project/tracker/repositories/DeveloperRepository.java
@@ -2,9 +2,12 @@ package com.project.tracker.repositories;
 
 
 import com.project.tracker.models.Developer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Integer> {
+    Page<Developer> findAll(Pageable pageable);
 }
